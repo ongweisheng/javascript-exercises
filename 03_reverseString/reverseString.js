78c179b58aee968a48b6c29843f85faa0f6c0b70
ongweisheng <ongweisheng30@gmail.com>
2022-08-15 10:11:03 +0800

    Add code for reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reverseArray = splitString.reverse();
+    let reverseString = reverseArray.join("");
+    return reverseString;
 };
 
 // Do not edit below this line
